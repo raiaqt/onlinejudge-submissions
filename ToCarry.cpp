dbcbf023f28f0d6f846a824761040255f580fb20
rsquitoriano <rsquitoriano@gmail.com>
2023-03-17 17:47:47 +0800

    bit manipulation

--- ToCarry.cpp
+++ ToCarry.cpp
@@ -1,21 +1,10 @@
-// 10469
-
 #include <cstdio>
 
-bool isOne(unsigned int num, int bit) {
-    return num & (1 << bit);
-}
-
 int main() {
-    unsigned int A, B, C;
+    int A, B, C;
     
     while (scanf("%d %d", &A, &B) != EOF) {
-        C = 0;
-        for (int i = 0; i < 32; i++) {
-            if (isOne(A, i) != isOne(B, i)) {
-                C += 1 << i;
-            }
-        }
+        C = A ^ B;
         printf("%d\n", C);
     }
     
